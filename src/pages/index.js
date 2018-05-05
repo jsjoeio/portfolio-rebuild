9989f7424f64fd21d89e66718fbb8edc19289eeb
Joe Previte <jjprevite@gmail.com>
2018-05-05 13:25:19 -0700

    fix icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,6 +45,10 @@ const Nav = styled.nav`
   justify-content: space-between;
 `;
 
+const Emoji = styled.span`
+  margin-right: 0.5rem;
+`;
+
 const IndexPage = props => {
   const postEdges = props.data.allMarkdownRemark.edges;
 
@@ -56,41 +60,31 @@ const IndexPage = props => {
         <Nav>
           <Link to="/contact">
             <Button big>
-              <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-              </svg>
+              <Emoji>💼</Emoji>
               Portfolio
             </Button>
           </Link>
           <Link to="/contact">
             <Button big>
-              <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-              </svg>
+              <Emoji>📚</Emoji>
               Bookshelf
             </Button>
           </Link>
           <Link to="/contact">
             <Button big>
-              <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-              </svg>
+              <Emoji>🧘‍♂️</Emoji>
               Meditation
             </Button>
           </Link>
           <Link to="/contact">
             <Button big>
-              <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-              </svg>
-              Music
+              <Emoji>🎧</Emoji>
+              Beats
             </Button>
           </Link>
           <Link to="/contact">
             <Button big>
-              <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
-              </svg>
+              <Emoji>📮</Emoji>
               Contact
             </Button>
           </Link>
