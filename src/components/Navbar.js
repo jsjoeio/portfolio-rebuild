58306e40ead886f53bc5a7cf722302ddce517dc3
Joe Previte <jjprevite@gmail.com>
2018-05-30 17:23:15 -0700

    hide navbar on tablet and mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,18 @@ import Link from 'gatsby-link'
 import styled from 'styled-components'
 import Button from '../components/Button'
 
+import { media } from '../utils/media'
+
 const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
+  @media ${media.tablet} {
+    display: none;
+  }
+
+  @media ${media.phone} {
+    display: none;
+  }
 `
 
 const Emoji = styled.span`
