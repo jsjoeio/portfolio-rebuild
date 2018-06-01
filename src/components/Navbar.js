474c6ef20ad126ac822cd1b53c9cb2adc7b54b9a
Joe Previte <jjprevite@gmail.com>
2018-05-31 17:17:28 -0700

    move navbar inside media query

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import Responsive from 'react-responsive'
+import MediaQuery from 'react-responsive'
 import styled from 'styled-components'
 import Button from '../components/Button'
 
@@ -17,39 +17,41 @@ const Nav = styled.nav`
 const Emoji = styled.span`
   margin-right: 0.5rem;
 `
-const Navbar = props => (
-  <Nav>
-    <Link to="/portfolio">
-      <Button big>
-        <Emoji>💼</Emoji>
-        Portfolio
-      </Button>
-    </Link>
-    <Link to="/bookshelf">
-      <Button big>
-        <Emoji>📚</Emoji>
-        Bookshelf
-      </Button>
-    </Link>
-    <Link to="/meditation">
-      <Button big>
-        <Emoji>🧘‍♂️</Emoji>
-        Meditation
-      </Button>
-    </Link>
-    <Link to="/beats">
-      <Button big>
-        <Emoji>🎧</Emoji>
-        Beats
-      </Button>
-    </Link>
-    <Link to="/contact">
-      <Button big>
-        <Emoji>📨</Emoji>
-        Contact
-      </Button>
-    </Link>
-  </Nav>
+const Navbar = () => (
+  <MediaQuery minDeviceWidth={992}>
+    <Nav>
+      <Link to="/portfolio">
+        <Button big>
+          <Emoji>💼</Emoji>
+          Portfolio
+        </Button>
+      </Link>
+      <Link to="/bookshelf">
+        <Button big>
+          <Emoji>📚</Emoji>
+          Bookshelf
+        </Button>
+      </Link>
+      <Link to="/meditation">
+        <Button big>
+          <Emoji>🧘‍♂️</Emoji>
+          Meditation
+        </Button>
+      </Link>
+      <Link to="/beats">
+        <Button big>
+          <Emoji>🎧</Emoji>
+          Beats
+        </Button>
+      </Link>
+      <Link to="/contact">
+        <Button big>
+          <Emoji>📨</Emoji>
+          Contact
+        </Button>
+      </Link>
+    </Nav>
+  </MediaQuery>
 )
 
 export default Navbar
