4245efa9601f415415500b1315560fec2a3eb014
Joe Previte <jjprevite@gmail.com>
2018-06-03 16:19:03 -0700

    clean up

--- src/components/MobileNavbar.js
+++ src/components/MobileNavbar.js
@@ -1,24 +1,9 @@
 import React, { Component } from 'react'
-import Link from 'gatsby-link'
 import MediaQuery from 'react-responsive'
 import styled from 'styled-components'
-import Button from '../components/Button'
 import MenuToggle from './MenuToggle'
 import MobileMenu from './MobileMenu'
 
-// import { media } from '../utils/media'
-
-// look at this https://www.npmjs.com/package/react-responsive
-// TODO: finish adding a ternary that says on desktop do this, else do this.
-
-const DesktopMenu = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const Emoji = styled.span`
-  margin-right: 0.5rem;
-`
 class Navbar extends Component {
   constructor(props, context) {
     super(props, context)
