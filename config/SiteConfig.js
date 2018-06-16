0094365d43673dfa2be7e3f2ecc60962d9cfea11
Joe Previte <jjprevite@gmail.com>
2018-06-15 19:19:59 -0700

    update siteConfig

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,23 +2,23 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
   siteTitle: 'Joe Previte', // Navigation and Site Title
-  siteTitleAlt: 'Minimal Blog - Gatsby Starter', // Alternative Site title for SEO
-  siteUrl: 'https://minimal-blog.netlify.com', // Domain of your site. No trailing slash!
+  siteTitleAlt: 'Joe Previte- frontend developer, mindfulness teacher and learner, // Alternative Site title for SEO',
+  siteUrl: 'https://www.joeprevite.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
-  siteBanner: '/social/banner.jpg',
-  siteDescription: 'Minimal Blog with big typography',
+  siteLogo: '/src/favicon.png', // Used for SEO and manifest
+  siteBanner: '/src/favicon.png',
+  siteDescription: 'a Scottsdale-based frontend developer, mindfulness teacher and learner',
 
   siteFBAppID: '123456789', // Facebook App ID
   userTwitter: 'jjprevite', // Twitter Username
-  ogSiteName: 'minimal', // Facebook Site Name
+  ogSiteName: 'joeprevite', // Facebook Site Name
 
   // Manifest and Progress color
-  themeColor: '#3498DB',
+  themeColor: '#DB504A',
   backgroundColor: '#2b2e3c',
 
   // Settings for typography.js
   headerFontFamily: 'Bitter',
   bodyFontFamily: 'Open Sans',
   baseFontSize: '18px',
-};
+}
